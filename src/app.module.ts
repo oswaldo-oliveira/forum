5d7c671e8f6fae752d16a42e37668e01c2833e60
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2023-12-23 13:37:46 -0300

    feat: create controller and service for create and authenticate a user.

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controller/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controller/authenticate.controller'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [PrismaService],
+})
+export class AppModule {}
