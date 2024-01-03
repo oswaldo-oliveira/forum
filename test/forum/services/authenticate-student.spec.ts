bded6822add3b4294f9358be977aa054e248a559
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-03 13:15:41 -0300

    test: create authenticate tests

---
+++ test/forum/services/authenticate-student.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryStudentsRepository } from '../repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { AuthenticateStudentService } from '@/domain/forum/application/services/authenticate-student'
+import { makeStudent } from '../factories/make-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+let sut: AuthenticateStudentService
+
+describe('Authenticate student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+    sut = new AuthenticateStudentService(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a student', async () => {
+    const student = makeStudent({
+      email: 'john.doe@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.items.push(student)
+
+    const result = await sut.execute({
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
