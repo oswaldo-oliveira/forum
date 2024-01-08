787a3877a745ca8e0c21b5fd0846ae021952f2f2
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-08 13:12:15 -0300

    refactor: replace interfaces to abstract classes

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,11 +1,11 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswerCommentsRepository {
-  create(answerComment: AnswerComment): Promise<void>
-  delete(answerComment: AnswerComment): Promise<void>
-  findById(answerCommentId: string): Promise<AnswerComment | null>
-  findManyByAnswerId(
+export abstract class AnswerCommentsRepository {
+  abstract create(answerComment: AnswerComment): Promise<void>
+  abstract delete(answerComment: AnswerComment): Promise<void>
+  abstract findById(answerCommentId: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(
     answerId: string,
     params: PaginationParams,
   ): Promise<AnswerComment[]>
