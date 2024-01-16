e4fab665ed09d9bf5c490fd4b916bea92bf646c2
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-16 18:44:46 -0300

    feat: creating a service for get a question comment and a answer comment with author

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,10 +1,16 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentsRepository {
   abstract create(questionComment: QuestionComment): Promise<void>
   abstract delete(questionComment: QuestionComment): Promise<void>
   abstract findById(questionCommentId: string): Promise<QuestionComment | null>
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
+
   abstract findManyByQuestionId(
     questionId: string,
     params: PaginationParams,
--- src/domain/forum/application/services/fetch-answer-comments.ts
+++ src/domain/forum/application/services/fetch-answer-comments.ts
@@ -1,7 +1,7 @@
 import { Either, right } from '@/core/either'
-import { AnswerComment } from '../../enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 import { Injectable } from '@nestjs/common'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 interface FetchAnswerCommentsServiceRequest {
   answerId: string
@@ -11,7 +11,7 @@ interface FetchAnswerCommentsServiceRequest {
 type FetchAnswerCommentsServiceResponse = Either<
   null,
   {
-    answerComments: AnswerComment[]
+    comments: CommentWithAuthor[]
   }
 >
 
@@ -23,11 +23,14 @@ export class FetchAnswerCommentsService {
     answerId,
     page,
   }: FetchAnswerCommentsServiceRequest): Promise<FetchAnswerCommentsServiceResponse> {
-    const answerComments =
-      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
-        page,
-      })
+    const comments =
+      await this.answerCommentsRepository.findManyByAnswerIdWithAuthor(
+        answerId,
+        {
+          page,
+        },
+      )
 
-    return right({ answerComments })
+    return right({ comments })
   }
 }
