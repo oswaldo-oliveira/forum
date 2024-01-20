9a8b10bf78f5671bf9df4e909d9499796eaae6d7
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-20 13:58:17 -0300

    feat: creating prisma question details

--- src/infra/database/prisma/mappers/prisma-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-attachment-mapper.ts
@@ -1,5 +1,6 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
-import { Prisma } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 
 export class PrismaAttachmentMapper {
   static toPrisma(
@@ -10,5 +11,15 @@ export class PrismaAttachmentMapper {
       title: attachment.title,
       url: attachment.url,
     }
+  }
+
+  static toDomain(attachment: PrismaAttachment): Attachment {
+    return Attachment.create(
+      {
+        title: attachment.title,
+        url: attachment.url,
+      },
+      new UniqueEntityID(attachment.id),
+    )
   }
 }
--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -5,6 +5,7 @@ import { PrismaService } from '../prisma.service'
 import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 import { Injectable } from '@nestjs/common'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
@@ -51,6 +52,24 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     }
 
     return PrismaQuestionMapper.toDomain(question)
+  }
+
+  async findDetailsBySlug(slug: string) {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+      include: {
+        author: true,
+        attachments: true,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionDetailsMapper.toDomain(question)
   }
 
   async create(question: Question) {
