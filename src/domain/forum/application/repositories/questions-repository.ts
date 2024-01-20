c23f3fe36eb046635635908ef669c3ff8a4c1efe
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-20 12:45:12 -0300

    feat: changing service and repository to return question details

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,10 +1,12 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract findById(questionId: string): Promise<Question | null>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract create(question: Question): Promise<void>
   abstract save(question: Question): Promise<void>
   abstract delete(questionId: Question): Promise<void>
--- src/domain/forum/application/services/get-question-by-slug.ts
+++ src/domain/forum/application/services/get-question-by-slug.ts
@@ -1,8 +1,8 @@
 import { Either, left, right } from '@/core/either'
-import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { Injectable } from '@nestjs/common'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 interface GetQuestionBySlugServiceRequest {
   slug: string
@@ -10,7 +10,7 @@ interface GetQuestionBySlugServiceRequest {
 
 type GetQuestionBySlugServiceResponse = Either<
   ResourceNotFoundError,
-  { question: Question }
+  { question: QuestionDetails }
 >
 
 @Injectable()
@@ -20,7 +20,7 @@ export class GetQuestionBySlugService {
   async execute({
     slug,
   }: GetQuestionBySlugServiceRequest): Promise<GetQuestionBySlugServiceResponse> {
-    const question = await this.questionRepository.findBySlug(slug)
+    const question = await this.questionRepository.findDetailsBySlug(slug)
 
     if (!question) {
       return left(new ResourceNotFoundError())
