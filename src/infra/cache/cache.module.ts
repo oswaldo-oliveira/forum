75b47d25dcc2319b5f9b5e4b17a91a4339633ce6
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-01-23 18:00:53 -0300

    feat: creating redis cache repository

---
+++ src/infra/cache/cache.module.ts
@@ -0,0 +1,15 @@
+import { EnvModule } from '@/infra/env/env.module'
+import { Module } from '@nestjs/common'
+import { CacheRepository } from './cache-repository'
+import { RedisCacheRepository } from './redis/redis-cache-repository'
+import { RedisService } from './redis/redis-service'
+
+@Module({
+  imports: [EnvModule],
+  providers: [
+    RedisService,
+    { provide: CacheRepository, useClass: RedisCacheRepository },
+  ],
+  exports: [CacheRepository],
+})
+export class CacheModule {}
